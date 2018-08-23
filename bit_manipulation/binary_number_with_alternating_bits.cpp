d3355052b975b46396fdabbdc82f0c972b304476
keith void <niangaotao@gmail.com>
2018-08-23 16:19:47 +0800

    add description

--- bit_manipulation/binary_number_with_alternating_bits.cpp
+++ bit_manipulation/binary_number_with_alternating_bits.cpp
@@ -1,3 +1,10 @@
+/*
+Given a positive integer, check whether it has alternating bits: namely, if 
+two adjacent bits will always have different values.
+*/
+
+
+
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
